5ff95f72afa8adb222796483079ca5eda9f067cc
Blackhatwhitehat <blackhatwhitehat999@gmail.com>
2024-11-03 13:38:25 -0600

    Created and debugged readfile() in uerd_create.py

--- userd_create.py
+++ userd_create.py
@@ -3,8 +3,27 @@
 #feel like learning how to do reading of files through BASH.
 # I am using this python script to that.  Deal with it.
 
+def readfile(filepath:str):
+    file = open(filepath, "r")
+    names=[]
+
+    for line in file:
+        name = ""
+        for char in line:
+            if char != "\n":
+                name = name + char
+        
+        names.append(name)
+
+    return names
+
+#This function is the main function in which calls all the other funtions
+#This a common practice in python scripts as a way to encapsulate the code
+#also it serves as a way to sand box the code a little better.
+#It also gives you as space to call custom functions without interfering with the rest of the code.
 def main():
-    print("Hello World")
+    print(readfile("authorized.raw"))
+
 
 if __name__=="__main__":
     main()
