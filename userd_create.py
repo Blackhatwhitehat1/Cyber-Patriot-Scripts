490aaca7c000b2eeb724bfa8792d49b617026f1a
Blackhatwhitehat <blackhatwhitehat999@gmail.com>
2024-11-03 12:56:53 -0600

    This is just a rename of userd_creat.py. I also created and debugged the main() function.

---
+++ userd_create.py
@@ -0,0 +1,10 @@
+# This file is responsible for creating the code that will run inside fo userd.sh
+# This is necessary because the user names are constantly changing and I do not
+#feel like learning how to do reading of files through BASH.
+# I am using this python script to that.  Deal with it.
+
+def main():
+    print("Hello World")
+
+if __name__=="__main__":
+    main()
